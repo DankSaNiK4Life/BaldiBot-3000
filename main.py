aee24e39da56018c58d956104679a8ee49b80b74
DankSaNiK4Life <kiddreece@hotmail.com>
2026-02-06 22:02:37 +0000

    Set location for .env

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv(".env")  # Load environment variables from .env file
 
 from discord_commands import bot
 import psutil
