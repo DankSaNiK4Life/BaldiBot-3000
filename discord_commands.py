168b4000dbe6bfa20167a411c02eef50cbeb7bce
DankSaNiK4Life <kiddreece@hotmail.com>
2026-02-11 10:51:54 +0000

    fixed it

--- discord_commands.py
+++ discord_commands.py
@@ -133,7 +133,7 @@ async def join(ctx):
             random_sound = get_random_sound()  # Start playing random sounds in the background
             cfg.voice_client.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=random_sound))
             print("Played Random Sound: " + random_sound)
-            await asyncio.sleep(random.randint(cfg.RANDOM_SOUND_INTERVAL)) # random interval between 5 minutes and 1 hour
+            await asyncio.sleep(random.randint(cfg.RANDOM_SOUND_INTERVAL[0], cfg.RANDOM_SOUND_INTERVAL[1])) # random interval between 5 minutes and 1 hour
     else:
         await ctx.send("You are not in a voice channel buddy!")
         print("The user is not in a channel")
