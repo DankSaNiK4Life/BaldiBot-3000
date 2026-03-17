e25d12122df54b08e9f265ea47575c7baa3e2be3
DankSaNiK4Life <kiddreece@hotmail.com>
2026-03-17 16:29:44 +0000

    removed error

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 
 from discord_commands import bot
-from voice_chat
  
 if __name__ == "__main__":
     bot.run(os.getenv("DISCORD_BOT_TOKEN")) # Starts the bot 
